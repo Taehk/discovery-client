ed22f064977566d7a4d9fbbb308e3f5454dca868
Taehk <taehk1013@gmail.com>
2024-08-04 20:18:15 +0900

    ss

--- src/main/java/com/playdata/discoveryClient/DiscoveryClientApplication.java
+++ src/main/java/com/playdata/discoveryClient/DiscoveryClientApplication.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 //@EnableDiscoveryClient
 @SpringBootApplication
 public class DiscoveryClientApplication {
-	
+	//
 	public static void main(String[] args) {
 		SpringApplication.run(DiscoveryClientApplication.class, args);
 	}
