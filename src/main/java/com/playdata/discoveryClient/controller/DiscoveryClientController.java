b8887de82b90b14852b8caf2196652c9be2a46eb
Taehk <taehk1013@gmail.com>
2024-08-05 11:31:11 +0900

    aaa

--- src/main/java/com/playdata/discoveryClient/controller/DiscoveryClientController.java
+++ src/main/java/com/playdata/discoveryClient/controller/DiscoveryClientController.java
@@ -12,7 +12,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class DiscoveryClientController {
     @RequestMapping(method = RequestMethod.GET)
     public ResponseEntity<String> healthCheck(){
-        return ResponseEntity.ok("Discovery Client Server Health Check-2");
+        return ResponseEntity.ok("Discovery Client Server Health Check-3");
     }
 
 }
